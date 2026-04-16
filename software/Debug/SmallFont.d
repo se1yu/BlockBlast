6f1c8bba194d9274591c73a431ba8ea2e33d8f67
se1yu <jeslynchang80@gmail.com>
2026-04-16 08:41:31 -0500

    default software file added

---
+++ software/Debug/SmallFont.d
@@ -0,0 +1,7 @@
+# FIXED
+
+SmallFont.o: ../SmallFont.cpp \
+ C:/Users/kayos/Documents/MSPM0_ValvanoWare/ECE319K_Lab9H/SmallFont.h \
+ ../../inc/ST7735.h
+C:/Users/kayos/Documents/MSPM0_ValvanoWare/ECE319K_Lab9H/SmallFont.h:
+../../inc/ST7735.h:
